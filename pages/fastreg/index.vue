a19028b834adeae3a06e44aba64ce24230dd6a11
Lagumir <Legals92@mail.ru>
2020-10-23 23:05:36 +0300

    fastreg

---
+++ pages/fastreg/index.vue
@@ -0,0 +1,138 @@
+<template lang="html">
+  <div class="fastreg">
+    <form class="fastreg-form-all">
+      <div class="fastreg-form">
+        <div class="fastreg-form-block">
+          <input id="form-input" type="file" class="fastreg-form-input">
+          <label for="form-input" class="faster-form-label">
+            <div class="faster-form-foto">
+              <img src="../../static/img/camera.svg" alt="фото">
+            </div>
+            <label class="faster-form-label-foto">Загрузите ваше фото</label>
+          </label>
+        </div>
+      </div>
+      <div class="fastreg-form1">
+        <div class="faster-form-block1">
+          <label class="faster-form-name">ФИО*</label>
+          <input type="text" class="faster-form-input-name" placeholder="Введите ваше ФИО">
+          <div class="faster-form-email-password">
+            <div>
+              <label class="faster-form-email">Почта*</label>
+              <br>
+              <input type="text" class="faster-form-input-email" placeholder="Введите вашу почту">
+            </div>
+            <div>
+              <label class="faster-form-password">Пароль*</label>
+              <br>
+              <input type="text" class="faster-form-input-password" placeholder="Введите пароль">
+            </div>
+          </div>
+          <div class="fastreg-form-chek">
+            <input type="checkbox">
+            <label>Я принимаю условия <span class="faster-form-chek-link">Пользовского соглашения</span></label>
+          </div>
+          <button type="submit" name="button" class="faster-form-reg-button">
+            Регистрация
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.fastreg{
+  width: 856px;
+  margin: 0 auto;
+}
+.fastreg-form-all{
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+.fastreg-form{
+  width: 282px;
+  height: 310px;
+  position: relative;
+
+}
+.fastreg-form1{
+  width: 540px;
+}
+.fastreg-form-block{
+  height: 100%;
+  background-color: #F2F2F2;
+}
+.fastreg-form-input{
+  opacity: 0;
+}
+.faster-form-label{
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+}
+.faster-form-foto{
+  width: 31%;
+  height: 28%;
+  margin: 0 auto;
+  margin-top: 57px;
+}
+.faster-form-label-foto{
+  width: 100%;
+  color: #6C757D;
+  text-align: center;
+}
+.faster-form-input-name{
+  width: 540px;
+  height: 46px;
+  border: 1px solid #CED4DA;
+  box-sizing: border-box;
+  border-radius: 4.8px;
+  padding-left: 16px;
+}
+.faster-form-email-password{
+  margin-top: 25px;
+  display: flex;
+  justify-content: space-between;
+}
+.faster-form-input-email{
+  width: 258px;
+  height: 46px;
+  border: 1px solid #CED4DA;
+  box-sizing: border-box;
+  border-radius: 4.8px;
+  padding-left: 16px;
+}
+.faster-form-input-password{
+  width: 258px;
+  height: 46px;
+  border: 1px solid #CED4DA;
+  box-sizing: border-box;
+  border-radius: 4.8px;
+  padding-left: 16px;
+}
+.fastreg-form-chek{
+  margin-top: 20px;
+}
+.faster-form-chek-link{
+  cursor: pointer;
+  color:#2D9CDB;
+  text-decoration: underline;
+}
+.faster-form-reg-button{
+  width: 540px;
+  height: 50px;
+  margin-top: 26px;
+  outline: 0;
+  color:#fff;
+  background-color: #84CD8B;
+  border: 0;
+  font-size: 20px;
+}
+</style>
---
+++ pages/formreg/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="form-reg">
+    <nav>
+      <div class="form-logo-link">
+        <nuxt-link to="/">
+          <img src="../../static/img/main-logo2.svg" alt="vd">
+        </nuxt-link>
+      </div>
+    </nav>
+    <section>
+      <div class="form-reg-fast">
+        <div class="form-reg-link">
+          <nav>
+            <div class="fast-reg" :class="{ active : isActive }" @click="isActive = true">
+              Быстрая регистрация
+            </div>
+          </nav>
+          <nav>
+            <div class="full-reg" :class="{ active : !isActive }" @click="isActive = false">
+              Полная регистрация
+            </div>
+          </nav>
+        </div>
+        <fastreg v-if="isActive" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+import Fastreg from '../fastreg/index'
+
+export default {
+  components: {
+    Fastreg
+  },
+  data () {
+    return {
+      isActive: false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.form-reg{
+  width: 952px;
+  margin: 0 auto;
+  margin-top: 112px;
+}
+.form-logo-link{
+  width: 20%;
+  margin: 0 auto;
+}
+.form-reg-fast{
+  width: 100%;
+  height: 492px;
+  border: 2px solid #DFDFDF;
+  margin-top: 56px;
+  margin-bottom: 56px;
+}
+.form-reg-link{
+  width: 70%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 48px;
+}
+.fast-reg{
+font-style: normal;
+font-weight: normal;
+font-size: 32px;
+line-height: 46px;
+display: flex;
+align-items: flex-end;
+color:  rgba(33, 37, 41, 0.4);
+cursor: pointer;
+}
+.fast-reg:hover{
+  color:#84CD8B;
+  border-bottom: 2px solid #84CD8B;
+}
+.full-reg{
+font-style: normal;
+font-weight: normal;
+font-size: 32px;
+line-height: 46px;
+display: flex;
+align-items: flex-end;
+color:  rgba(33, 37, 41, 0.4);
+cursor: pointer;
+margin-bottom: 20px;
+}
+.full-reg:hover{
+  color:#84CD8B;
+  border-bottom: 2px solid #84CD8B;
+}
+.active{
+  color:#84CD8B;
+  border-bottom: 2px solid #84CD8B;
+}
+</style>
