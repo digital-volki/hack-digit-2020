6f20c6a9ed006934c0f033c4cee4c2fdbdad68b4
vlad <shkerdin.v@ya.ru>
2020-10-24 00:16:50 +0300

    delete unused cm

--- pages/dash/index.vue
+++ pages/dash/index.vue
@@ -6,18 +6,15 @@
     <div class="row">
       <Card v-for="i in 24" :key="i" />
     </div>
-    <Comments />
   </div>
 </template>
 
 <script>
-import Comments from '../../components/dash/Comments'
 import Card from '~/components/dash/Card'
 
 export default {
   name: 'Dash',
   components: {
-    Comments,
     Card
   },
   layout: 'dash'
