9c46772e91a16e895e6202be24d08841968aaa0b
a.timoshin <killfreedom@yandex.ru>
2020-10-24 05:28:08 +0300

    Просмотр курса, сайдбар

---
+++ layouts/DashCourse.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <div class="col-auto">
+      <LeftSidebar />
+    </div>
+    <div class="p-5 pr-0 col-auto">
+      <Nuxt />
+    </div>
+    <div class="col m-0 p-0">
+      <RightSideBar />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .row{
+    width: 100.8vw;
+  }
+</style>
+<script>
+import RightSideBar from '../components/dash/RightSideBar'
+import LeftSidebar from '~/components/dash/LeftSidebar'
+
+export default {
+  components: { LeftSidebar, RightSideBar }
+}
+</script>
